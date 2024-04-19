0e88ee8e22972cf996281c722b4f83e5d5294ab7
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-04-19 10:29:59 -0500

    added read function

--- create_file.php
+++ create_file.php
@@ -14,6 +14,8 @@ function command()
 		echo "the user response was 2";
 		command();
 	} else if ($user_response == 3) {
+		read_file();
+	} else if ($user_response == 4) {
 		exit();
 	} else {
 		echo "not an acceptable input";
@@ -30,9 +32,26 @@ function create_file()
 
 	$full_path = $folder_path . DIRECTORY_SEPARATOR . $file_name;
 
-	file_put_contents($full_path, "example content");
+	echo "Enter file contents: ";
+	$file_contents = trim(fgets(STDIN));
+
+	file_put_contents($full_path, $file_contents);
 
 	echo "File '$file_name' created successfully.";
+}
+
+function read_file()
+{
+	echo "What file do you want to see the contents of?\n";
+	$file_name = trim(fgets(STDIN));
+
+	$folder_path = "files_output";
+	$full_path = $folder_path . DIRECTORY_SEPARATOR . $file_name;
+
+	$handle = fopen($full_path, "r");
+	$contents = fread($handle, filesize($full_path));
+	echo $contents;
+	fclose($handle);
 }
 
 function read_options()
@@ -41,7 +60,8 @@ function read_options()
 	echo "
 	1: Create New File
 	2: Rename File
-	3: Close App\n";
+	3: Read App
+	4: Close App\n";
 }
 
 function open_file()
